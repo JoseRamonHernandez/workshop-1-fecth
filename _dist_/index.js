e6a9bda3a59027868913b1b1d25c95650fa2388f
JoseRamonHernandez <JoseRamonHernandez@users.noreply.github.com>
2022-01-29 03:33:22 +0000

    deploy: 7f9886f98567cb4ad12a4b93a74c439a143d7915

--- _dist_/index.js
+++ _dist_/index.js
@@ -5,6 +5,13 @@
 
 const baseUrl ="https://platzi-avo.vercel.app";
 const appNode = document.querySelector('#app')
+const formatPrice = (price) => {
+  const newPrice =  new window.Intl.NumberFormat('en-EN', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(price);
+    return newPrice;
+};
 
 window
 .fetch(`${baseUrl}/api/avo`)
@@ -15,13 +22,21 @@ window
         //crer imagen
         const imagen = document.createElement("img");
         imagen.src = `${baseUrl}${item.image}`;
+        imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
         //crear titulo
         const title = document.createElement("h2");
         title.textContent = item.name;
+        title.className = "text-lg";
         //crear precio
         const price = document.createElement("div");
-        price.textContent = item.price;
-    
+        price.className = "text-gray-600"
+        price.textContent = formatPrice(item.price);
+        
+            const priceAndTitle = document.createElement("div");
+            priceAndTitle.className = "text-center md:text-left";
+            priceAndTitle.appendChild(title);
+            priceAndTitle.appendChild(price);
+
             const container = document.createElement("div");
             container.append(imagen, title, price);
             todosLosItems.push(container);
